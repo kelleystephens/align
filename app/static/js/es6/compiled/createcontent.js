9947f1dde2f00bf2f6cfdd5a015e05567d33b316
Kelley Stephens <kelley.stephens@outlook.com>
2014-05-24 13:22:37 -0500

    changes needed to be commited to merge

---
+++ app/static/js/es6/compiled/createcontent.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  $(document).ready(init);
+  function init() {
+    $('.add-content').click(addCard);
+    $('form#content').on('click', '.delete', deleteCard);
+  }
+  function deleteCard(e) {
+    if ($('form#content > .content-item').length > 1) {
+      $(this).parent().remove();
+    }
+    e.preventDefault();
+  }
+  function addCard() {
+    var card = $('#content > .content-item');
+    $('#content').append(card.clone());
+  }
+})();
+
+//# sourceMappingURL=createcontent.map
